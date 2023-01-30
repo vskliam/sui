63e03fec2f4ee340323ed63b3356d2ca2737ba00
vskliam <vskliam@users.noreply.github.com>
2023-01-30 10:00:32 +0000

    deploy: 0a5b3a79175753b373a5b1e2bf5d000350a3b449

---
+++ sui_types/error/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"constant":[["TRANSACTION_NOT_FOUND_MSG_PREFIX",""]],"enum":[["SuiError","Custom error type for Sui."]],"fn":[["convert_vm_error",""]],"struct":[["ExecutionError",""],["MoveVM",""]],"type":[["ExecutionErrorKind",""],["SuiResult",""]]};
